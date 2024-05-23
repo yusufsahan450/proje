04d7e9538bcdedd9a08f01137f91ae5cd60cee24
yusufsahan450 <yusufsahan4627@gmail.com>
2024-05-23 21:29:29 +0300

    -yaş toplama fonksiyonu eklendi

--- print.py
+++ print.py
@@ -1,1 +1,17 @@
-print("yusuf") 
+isim = "selim" 
+print(isim)
+isim = "yusuf"
+print (isim)
+isim2 = "selim"
+print(isim2)
+
+yas = 13
+print("{}un yası {}".format(isim,yas))
+
+yas2 = int(input("yaşınız:"))
+print("{}in yası {}".format(isim2,yas2))
+
+def yasToplama(a,b):
+    yaslarinToplami =a+b
+    print("{}le {}un yasları toplamı {}".format(isim2,isim,yaslarinToplami))
+yasToplama(yas,yas2)
